32f8249be595d2de11c830f9e9a3a943aee9de6f
João Demari <joaodemari1@gmail.com>
2024-05-21 09:41:05 -0300

    trying cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   await app.listen(3000);
 
   if (module.hot) {
